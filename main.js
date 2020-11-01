0e1faf4498c5412b9dc158437f2626ec5f227505
Tomasz Kos <kos.tomasz.mateusz@gmail.com>
2020-11-01 16:36:06 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const name = 'Tomek';
+const age = 20;
+
+//console.log(name);
+//console.log(age);
+console.log(`Witam Cię na mojej biedackiej stronie. Jestem ${name}. Mam ${age} lat. I mam nadzieję, że w końcu posiądę tajemną wiedzę frontendu..`);
+
+//alert('siema byczq');
