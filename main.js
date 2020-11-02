8069543dc4fe8c0c1c442f7ff4182f4a20696ecf
Tomasz Kos <kos.tomasz.mateusz@gmail.com>
2020-11-02 21:24:04 +0100

    IV week finished: empty element filled by using js

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const name = 'Tomek';
+const name = "Tomek";
 const age = 20;
 
 //console.log(name);
 //console.log(age);
-console.log(`Witam Cię na mojej biedackiej stronie. Jestem ${name}. Mam ${age} lat. I mam nadzieję, że w końcu posiądę tajemną wiedzę frontendu..`);
+// alert('halo halo');
+console.log(
+  `Witam Cię na mojej biedackiej stronie. Jestem ${name}. Mam ${age} lat. I mam nadzieję, że w końcu posiądę tajemną wiedzę z dziedziny frontendu.`
+);
 
-//alert('siema byczq');
+const realHeading = document.querySelector(".header__real-article--js");
+
+realHeading.innerHTML = `Strona przedstawia niezobowiązujące, pisane w luźnym tonie nowinki o ekipie z Madrytu. </br> PS: ten tekst pisałem JS'em tylko dlatego, że miałem takie zadanie.`;
+
+// const studyHeading = document.querySelector(".header__study-article--js");
+// studyHeading.innerHTML = `Najpierw mówi, że to co się da robić za pomocą HTML'a robimy HTML'em, a teraz każe pisać <strong> ten </strong> tekst za pomocą JS'a..`;
+// Odkryłem, że musiałbym utworzyć i podpiąć drugi plik .js do podstrony, więc póki co przeżyję z jednym.. 
