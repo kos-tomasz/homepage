3f0ab9b4111c9c5a98c38734acf31657876247d5
Tomasz Kos <kos.tomasz.mateusz@gmail.com>
2020-11-07 14:55:59 +0100

    greeting functions added

--- main.js
+++ main.js
@@ -4,14 +4,23 @@ const age = 20;
 //console.log(name);
 //console.log(age);
 // alert('halo halo');
-console.log(
-  `Witam Cię na mojej biedackiej stronie. Jestem ${name}. Mam ${age} lat. I mam nadzieję, że w końcu posiądę tajemną wiedzę z dziedziny frontendu.`
-);
+
+function greet(name, age) {
+  console.log(
+    `Witam Cię na mojej biedackiej stronie. Jestem ${name}. Mam ${age} lat. I mam nadzieję, że w końcu posiądę tajemną wiedzę z dziedziny frontendu.`
+  );
+}
+greet(name, age);
+
+// FAT arrow function:
+const secondGreet = (name, age) => {
+  console.log(`Witam Cię na mojej biedackiej stronie. Jestem ${name}. Mam ${age} lat. I mam nadzieję, że w końcu posiądę tajemną wiedzę z dziedziny frontendu.`);
+}
+secondGreet('Zbysiu', 34);
 
 const realHeading = document.querySelector(".header__real-article--js");
-
 realHeading.innerHTML = `Strona przedstawia niezobowiązujące, pisane w luźnym tonie nowinki o ekipie z Madrytu. </br> PS: ten tekst pisałem JS'em tylko dlatego, że miałem takie zadanie.`;
 
 // const studyHeading = document.querySelector(".header__study-article--js");
 // studyHeading.innerHTML = `Najpierw mówi, że to co się da robić za pomocą HTML'a robimy HTML'em, a teraz każe pisać <strong> ten </strong> tekst za pomocą JS'a..`;
-// Odkryłem, że musiałbym utworzyć i podpiąć drugi plik .js do podstrony, więc póki co przeżyję z jednym.. 
+// Odkryłem, że musiałbym utworzyć i podpiąć drugi plik .js do podstrony, więc póki co przeżyję z jednym..
